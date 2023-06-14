04744f6b9345c8aea0ff10fa3aebaaa9b08c2280
Tatev Melikyan <tatev.melikyan@ymail.com>
2023-06-15 00:40:41 +0400

    Changed BrouserRouter to HashRouter for github pages to work.

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
       <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
 );
 
